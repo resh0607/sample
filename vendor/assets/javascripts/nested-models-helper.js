123d691d7441515376844490062ca83488bf0232
Ivan Reshetnyak <reshetnyak.ivan@rt.ru>
2024-11-19 13:12:03 +0300

    sample

---
+++ vendor/assets/javascripts/nested-models-helper.js
@@ -0,0 +1,31 @@
+(function($) {
+  // Создание вложенной модели
+  $(document).on ("click", ".create-nested-model", function () {
+    // Формируем модель из шаблона
+    var new_model = $("#" + $(this).data ("blueprint")).tmpl ({
+        association : $(this).data ("association")
+      , id          : new Date ().getTime ()
+    });
+
+    // Вычисляем плейсхолдер (по умолчанию добавляем модель сразу после ссылки)
+    var placeholder = $($(this).data ("placeholder") || $(this).parent ());
+
+    // Добавляем модель на страницу и отправляем событие форме
+    new_model.hide ().appendTo (placeholder).closest ("form").trigger ("nested:modelAdded");
+    new_model.slideDown ();
+  });
+
+  // Удаление вложенной модели
+  $(document).on ("click", ".destroy-nested-model", function () {
+    // Получаем модель по её имени
+    var model = $(".nested-model[data-model='" + $(this).data ("association") + "']");
+
+    // Проставляем флаг удаления и отправляем событие форме
+    model.find("input[name$='[_destroy]']")
+      .val ("1")
+      .closest ("form").trigger ("nested:modelRemoved");
+
+    // Скрываем модель
+    model.slideUp ("fast");
+  });
+}) (jQuery);
